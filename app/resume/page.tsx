6dd73ddd399e379be5808c5af7e2305b87bf8712
evancosmos <evanclark@live.ca>
2024-08-04 19:43:37 -0700

    added firebase hosting, fixed pdf deps

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,7 +5,9 @@ import { pdfjs, Document, Page} from 'react-pdf';
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+const punycode = require('punycode/');
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
 
 export default function Resume() {
     const [dynamicInvert, setDynamicInvert] = useState<string>("");
